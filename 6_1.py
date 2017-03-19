c03ea4638e9c212d19d3c49edafe5fdd3e25cb53
DimaT1 <dymych99@gmail.com>
2017-03-19 22:10:55 +0300

    Update 6_1.py

--- 6_1.py
+++ 6_1.py
@@ -2,14 +2,15 @@ import re
 
 f1 = open("in.txt", 'r', encoding='utf-8')
 
-sentences = []
+max_sentences = ['', '', '']
 
 for line in f1:
-    sentences += re.findall(r'[^\.\!\?]+[\.\!\?]', line)
+    line_sentences = re.findall(r'[^\.\!\?]+[\.\!\?]', line)
+    for one_line_sentence in line_sentences:
+        if len(one_line_sentence.split()) > len(max_sentences[2].split()):
+            max_sentences[2] = one_line_sentence
+            max_sentences.sort(key=lambda one_sentence: len(one_sentence.split()), reverse=True)
 
-sentences.sort(key=lambda one_sentence: len(one_sentence.split()), reverse=True)
-print(sentences[0])
-print()
-print(sentences[1])
-print()
-print(sentences[2])
+print(max_sentences[0], end='\n\n')
+print(max_sentences[1], end='\n\n')
+print(max_sentences[2])
