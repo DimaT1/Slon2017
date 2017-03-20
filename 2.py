1410dd6e967a1630e2c516a1041bd5fc42e69d89
DimaT1 <dymych99@gmail.com>
2017-03-20 23:09:47 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 s = input()
 for i in ['ё', 'у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю', 'Ё', 'У', 'Е', 'Ы', 'А', 'О', 'Э', 'Я', 'И', 'Ю']:
-    s = s.replace(i, i + 'с' + i)
+    s = s.replace(i, i + 'с' + i.lower())
 s = s.replace('р', 'л')
 s = s.replace('Р', 'Л')
 print(s)
